7f9a3c2976be227461738d12ad8989946589ea31
AbdulQudus01 <abdulquduskasali@gmail.com>
2022-11-10 22:25:54 -0800

    Header File

---
+++ main.h
@@ -0,0 +1,100 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <limits.h>
+
+#define BUFSIZE 1024
+#define TOK_BUFSIZE 128
+#define TOK_DELIM " \t\r\n\a"
+
+/* Points to an array of pointer to strings called the environment */
+extern char **environ;
+
+/**
+ * struct data -struct that contains all relevant data on runtime
+ * @av: Argument vector
+ * @input: Command line writtern by the user
+ * @args: Tokens of the command line
+ * @status: Last status of the shell
+ * @counter: Lines counter
+ * @_environ: Environment variable
+ * @pid: process ID of the shell
+ */
+typedef struct data
+{
+	char **av;
+	char *input;
+	char **args;
+	int status;
+	int counter;
+	char **_environ;
+	char *pid;
+} data_shell;
+
+/**
+ * struct sep_list_s - single linked list
+ * @separator: ; | &
+ * @next: Next node
+ * Description: single linked list to store separators
+ */
+typedef struct sep_list_s
+{
+	char separator;
+	struct sep_list_s *next;
+} sep_list;
+
+/**
+ * struct line_list_s - single linked list
+ * @line: Command line
+ * @next: Next code
+ * Description: Single linked list to store command lines
+ */
+typedef struct line_list_s
+{
+	char *line;
+	struct line_list_s *next;
+} line_list;
+
+/**
+ * struct r_var_list - Single linked list
+ * @len_var: Length of the variable
+ * @val: Value of the variable
+ * @len_val: Length of the value
+ * @next: Next node
+ * Description: Single linked list to store variables
+ */
+typedef struct r_var_list
+{
+	int len_var;
+	char *val;
+	int len_val;
+	struct r_var_list *next;
+} r_var;
+
+/**
+ * struct builtin_s - Builtin struct for command args.
+ * @name: The name of the command builtin i.e cd, exit, env
+ * @f: Data type pointer function
+ */
+typedef struct builtin_s
+{
+	char *name;
+	int (*f)(data_shell *datash);
+} builtin_t;
+
+/* aux_lists.c */
+sep_list *add_sep_node_end(sep_list **head, char sep);
+void free_sep_list(sep_list **head);
+line_list *add_line_node_end(line_list **head, char *line);
+void free_line_list(line_list **head);
+
+/* aux_lists2.c */
